ce3c3e0b6f63bd67e01ad51706dad2c0443a6afe
Savestate2a03 <joseph.elkhouri+github@gmail.com>
2021-07-02 15:10:28 +0200

    restructure

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='deerbot-dev',
+      version='0.0.1a',
+      description='deerbot-dev development bot',
+      author='Savestate',
+      author_email='joseph.elkhouri@gmail.com',
+      packages=['python', 'requests'],
+     )
