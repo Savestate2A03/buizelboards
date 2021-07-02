0ef11e30d5cd0b6fec9abef551cb673cf145114f
Savestate2a03 <joseph.elkhouri+github@gmail.com>
2021-07-02 15:45:38 +0200

    more restructuring

--- deerbot_dev/setup.py
+++ deerbot_dev/setup.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
-
-setup(name='deerbot-dev',
-      version='0.0.1a',
-      description='deerbot-dev development bot',
-      author='Savestate',
-      author_email='joseph.elkhouri@gmail.com',
-      packages=['deerbot_dev', 'deerbot_dev.commands'],
-     )
+setuptools.setup(
+    name="deerbot-dev",
+    version="0.0.1a",
+    author="Savestate",
+    author_email="joseph.elkhouri@gmail.com",
+    description="Personal Discord development bot for friends' servers",
+    url="https://github.com/Savestate2A03/deerbot-dev",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
